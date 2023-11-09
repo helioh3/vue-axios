a95416834f50f3af8ea6785295bea5398333276d
Helio Brito <helioh3@gmail.com>
2023-11-09 16:52:58 -0300

    feature: implement of request get

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <pre>
+      <button @click="get">Start</button>
+    </pre>
   </main>
 </template>
+
+<script>
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+export default {
+  setup () {
+    const config = {
+      params: {
+        _limit: 5
+      }
+    }
+
+    const data = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    }
+
+    const get = async () => {
+      try {
+        const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts', config)
+      } catch (error) {
+        throw new Error(error)
+      }
+
+      // try {
+      //   const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5', {})
+      // }catch (error) {
+      //   throw new Error(error)
+      // }
+    }
+  }
+}
+</script>
